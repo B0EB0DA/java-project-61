d88f157b4618d6b1fa79b02bfc13722f059fe021
B0EB0DA <GordienkoM@gmail.com>
2023-09-20 22:40:18 +0300

    Final in project 1.4 - Cycle in task selection excluded

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -8,36 +8,33 @@ public class Engine {
     private static final Scanner consoleScanner = new Scanner(System.in);
     static void theInterface() {
 
-        while (true) {
-            System.out.println("Please enter the game number and press Enter.");
-            System.out.println("1 - Greet");
-            System.out.println("2 - Even");
-            System.out.println("3 - Calc");
-            System.out.println("4 - GCD");
-            System.out.println("5 - Progression");
-            System.out.println("6 - Prime");
-            System.out.println("0 - Exit");
-            System.out.print("Your choice: ");
-            int intAnswer = consoleScanner.nextInt();
+         System.out.println("Please enter the game number and press Enter.");
+         System.out.println("1 - Greet");
+         System.out.println("2 - Even");
+         System.out.println("3 - Calc");
+         System.out.println("4 - GCD");
+         System.out.println("5 - Progression");
+         System.out.println("6 - Prime");
+         System.out.println("0 - Exit");
+         System.out.print("Your choice: ");
+         int intAnswer = consoleScanner.nextInt();
 
-            if (intAnswer == 0) break;
+         if (intAnswer > 0) {
 
-            if (userName == null) {
-                System.out.println();
-                System.out.println("Welcome to the Brain Games!");
-                System.out.print("May I have your name? ");
-                userName = consoleScanner.next();
-            }
-            System.out.println("Hello, " + userName + "!");
+             System.out.println();
+             System.out.println("Welcome to the Brain Games!");
+             System.out.print("May I have your name? ");
+             userName = consoleScanner.next();
+             System.out.println("Hello, " + userName + "!");
 
-            switch (intAnswer) {
-                case 2 -> theGame(new Even());
-                case 3 -> theGame(new Calc());
-                case 4 -> theGame(new GCD());
-                case 5 -> theGame(new Progression());
-                case 6 -> theGame(new Prime());
-            }
-        };
+             switch (intAnswer) {
+                 case 2 -> theGame(new Even());
+                 case 3 -> theGame(new Calc());
+                 case 4 -> theGame(new GCD());
+                 case 5 -> theGame(new Progression());
+                 case 6 -> theGame(new Prime());
+             }
+         }
         consoleScanner.close();
     }
 
