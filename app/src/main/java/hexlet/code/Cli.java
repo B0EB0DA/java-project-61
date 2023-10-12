7ab1e0085ab0b6e68aa64ee374fba3d89290aaad
B0EB0DA <GordienkoM@gmail.com>
2023-10-12 23:54:01 +0300

    Final in project 1.16 - Corrected mentor's objections v2

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 public class Cli {
     static void greeting(Scanner consoleScanner) {
+        System.out.print("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String answer = consoleScanner.next();
         System.out.println("Hello, " + answer + "!");
