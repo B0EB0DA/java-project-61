a4f05bf2586d83e0dca8af36fa321dad6d3fc4cd
B0EB0DA <GordienkoM@gmail.com>
2023-10-17 13:23:31 +0300

    Final in project 1.25 - Corrected mentor's objections v4

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -3,8 +3,6 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 import hexlet.code.Utils;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public final class Progression {
